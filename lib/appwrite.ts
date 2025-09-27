dd50e6db7a59977a3eb9a345560364e4a1da1918
Sadanand09 <sadanandgurude9@gmail.com>
2025-09-27 21:52:08 +0530

    ref:Implement Auth screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   Platform: "com.anand.fastfood",
+  databaseId: "68d6be630004d3fa58c0",
+  userCollectionId: "user",
 };
